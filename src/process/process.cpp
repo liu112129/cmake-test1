3d71c6d68a12e47b6f4006a51889f808cf5b5ef4
ly <ly@123.com>
2024-11-01 14:32:45 +0800

    第一次提交

---
+++ src/process/process.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "process.h"
+using std::cout,std::endl;
+
+void Process::planProcess(){
+
+    cout<<"this is a planning process"<<endl;
+    mymap.mapInfo();
+    cout<<"planning success"<<endl;
+
+}
